4294e95be81d2ef0e71c55a18a9a6dd02c2b1ccc
DJ-BoT07 <djmangaonkar7@gmail.com>
2024-09-05 18:10:57 +0530

    UI Done

---
+++ app/component/Content.jsx
@@ -0,0 +1,12 @@
+"use client";
+import { GlobeDemo } from "./Hero";
+
+function Content() {
+  return (
+    <div className="w-full h-screen flex items-center justify-center bg-black p-5">
+      <GlobeDemo />
+    </div>
+  )
+}
+
+export default Content
--- app/page.js
+++ app/page.js
@@ -1,7 +1,8 @@
-
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
 import Link from "next/link";
+import { GlobeDemo } from "./component/Hero";
+import Content from "./component/Content";
 // import { usePathname } from "next/navigation"; // Import the hook
 
 export default function Home() {
@@ -12,7 +13,8 @@ export default function Home() {
       <div>
         <div className="flex p-4 items-center justify-between bg-secondary shadow-sm bg-black">
           <Image src={"/favicon.ico"} alt="logo" width={50} height={60} className="filter invert"/>
-          <ul className="hidden md:flex gap-6 text-white">
+          
+          <ul className="hidden md:flex gap-6 text-white items-center">
             <Link
               href={"/"}
               className={`hover:text-primary hover:font-bold transition-all cursor-pointer hover:scale-110
@@ -37,12 +39,13 @@ export default function Home() {
             >
               404
             </Link>
+            <Button className="bg-white text-black px-6 font-bold py-5 rounded-md">Login</Button>
           </ul>
           {/* <UserButton /> */}
-          <Button className="bg-white text-black px-6 font-bold py-5 rounded-md">Login</Button>
         </div>
       </div>
       {/* <Hero /> */}
+      <Content />
     </>
   );
 }
